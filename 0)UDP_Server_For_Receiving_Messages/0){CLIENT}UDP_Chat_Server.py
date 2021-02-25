1fb79daa74c0212e900b9331719c20109462ba8a
mrHola21 <66013271+mrHola21@users.noreply.github.com>
2021-02-25 18:59:41 +0530

    Update 0){CLIENT}UDP_Chat_Server.py

--- 0)UDP_Server_For_Receiving_Messages/0){CLIENT}UDP_Chat_Server.py
+++ 0)UDP_Server_For_Receiving_Messages/0){CLIENT}UDP_Chat_Server.py
@@ -1,6 +1,6 @@
 import socket
 
-UDP_HOST = socket.gethostbyname(socket.gethostname())
+UDP_HOST = ''#run the code which is present at the end of the script
 UDP_PORT = 8080
 to = (UDP_HOST, UDP_PORT)
 while True:
@@ -8,3 +8,5 @@ while True:
 
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     s.sendto(message, to)
+    
+#print(socket.gethostbyname(socket.gethostname()))
