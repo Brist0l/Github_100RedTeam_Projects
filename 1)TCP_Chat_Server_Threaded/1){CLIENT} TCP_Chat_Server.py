7c02f27eb814be4819dbb8034ba27e4a7b58f742
mrHola21 <66013271+mrHola21@users.noreply.github.com>
2021-02-26 07:34:33 -0500

    Add files via upload

---
+++ 1)TCP_Chat_Server_Threaded/1){CLIENT} TCP_Chat_Server.py
@@ -0,0 +1,36 @@
+import socket
+import threading
+
+HOST = '192.168.51.1'
+PORT = 8080
+connect = (HOST, PORT)
+
+nickname=input("enter ur nickname:")
+
+client=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(connect)
+
+def receinve():
+    while True:
+        try:
+            message=client.recv(1024).decode('ascii')
+            if message=='NICK':
+                client.send(nickname.encode('ascii'))
+
+            else:
+                print(message)
+        except :
+            print("An error occured")
+            client.close()
+            break
+
+def write():
+    while True:
+        text=input("enter ur msg:")
+        message=f'{nickname}:{text}'
+        client.send(message.encode('ascii'))
+
+recv_thread=threading.Thread(target=receinve)
+recv_thread.start()
+write_thread=threading.Thread(target=write)
+write_thread.start()
