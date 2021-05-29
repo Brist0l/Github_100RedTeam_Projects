fbd51b62b42a6a69b43d69f62807b8931185b105
mrHola21 <66013271+mrHola21@users.noreply.github.com>
2021-05-28 23:29:36 -0700

    Add files via upload

--- 12)Port_scanner_OS/main.py
+++ 12)Port_scanner_OS/main.py
@@ -1,14 +1,14 @@
 from scapy.all import *
 from scapy.layers.inet import IP, ICMP
 
-pack = IP(dst="192.168.64.98") / ICMP()
+pack = IP(dst="100.65.181.155") / ICMP()
 resp = sr1(pack, timeout=2)
 
 if resp is None:
     print("No response")
 elif IP in resp:
     if resp.getlayer(IP).ttl <= 64:
-        theos = "Linux/Google's Customised Linux/FreeBSD"
+        theos = "Linux/Google's Customised Linux(Android)/FreeBSD"
     elif resp.getlayer(IP).ttl <= 128:
         theos = "Windows XP / Window10 / Windows 7 / Vista"
     elif resp.getlayer(IP).ttl <= 225:
