bb4e2891abb214333cf67f6b2b3a68ee4283e7d0
mrHola21 <66013271+mrHola21@users.noreply.github.com>
2021-05-28 22:52:53 -0700

    Add files via upload

---
+++ 15)Recursive_dir_brutefocers/main.py
@@ -0,0 +1,68 @@
+import queue
+import threading
+import urllib.error
+import urllib.parse
+import urllib.request
+
+threads = 50
+target_url = "http://testphp.vulnweb.com"
+wordlist_file = "all.txt"
+resume = None
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 " \
+             "Safari/537.36 OPR/75.0.3969.279 "
+
+
+def build_wordlist(wordlist_file):
+    # read in the word list
+    fd = open(wordlist_file, "r")
+    raw_words = [line.rstrip('\n') for line in fd]
+    fd.close()
+
+    found_resume = False
+    words = queue.Queue()
+
+    for word in raw_words:
+        if resume:
+            if found_resume:
+                words.put(word)
+            else:
+                if word == resume:
+                    found_resume = True
+                    print("Resuming wordlist from: %s" % resume)
+        else:
+            words.put(word)
+    return words
+
+
+def dir_bruter(extensions=None):
+    while not word_queue.empty():
+        attempt = word_queue.get()
+        attempt_list = []
+
+        if "." not in attempt:
+            attempt_list.append("/%s/" % attempt)
+        else:
+            attempt_list.append("/%s" % attempt)
+
+        if extensions:
+            for extension in extensions:
+                attempt_list.append("/%s%s" % (attempt, extension))
+
+        for brute in attempt_list:
+            url = "%s%s" % (target_url, urllib.parse.quote(brute))
+            try:
+                headers = {"User-Agent": user_agent}
+                r = urllib.request.Request(url, headers=headers)
+                response = urllib.request.urlopen(r)
+                if len(response.read()):
+                    print("[%d] => %s" % (response.code, url))
+            except urllib.error.HTTPError as e:
+                if e.code != 404:
+                    print("!!! %d => %s" % (e.code, url))
+                pass
+        dir_bruter()
+
+
+word_queue = build_wordlist(wordlist_file)
+file_extensions = [".php", ".bak", ".orig", ".inc"]
+dir_bruter(file_extensions)
